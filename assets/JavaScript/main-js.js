3f9cdef0a69577720ae635a0daf0b8103154be55
Dexter <ikfahkmas@gmail.com>
2025-08-10 17:14:18 +0700

    Initial commit

---
+++ assets/JavaScript/main-js.js
@@ -0,0 +1,6 @@
+let btnnav = document.getElementById("btn-nav").addEventListener("click",() => {
+    document.querySelector(".navbar").classList.toggle("active")
+})
+let btnnav2 = document.getElementById("btn-nav2").addEventListener("click",() => {
+    document.querySelector(".navbar").classList.toggle("active")
+})
