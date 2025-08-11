21fb0ccea89403f7bc0aaca9b7a800c2319b74b0
Dexter <ikfahkmas@gmail.com>
2025-08-11 12:50:11 +0700

    Menambahkan file baru dan update file lama

--- assets/JavaScript/main-js.js
+++ assets/JavaScript/main-js.js
@@ -4,3 +4,11 @@ let btnnav = document.getElementById("btn-nav").addEventListener("click",() => {
 let btnnav2 = document.getElementById("btn-nav2").addEventListener("click",() => {
     document.querySelector(".navbar").classList.toggle("active")
 })
+
+
+
+
+
+
+
+
