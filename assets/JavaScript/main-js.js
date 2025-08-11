6b887683e4583fa97397c046c01b53aff9f51e3d
WeRavens <ikfahkmas@gmail.com>
2025-08-11 12:57:27 +0700

    Add files via upload

---
+++ assets/JavaScript/main-js.js
@@ -0,0 +1,14 @@
+let btnnav = document.getElementById("btn-nav").addEventListener("click",() => {
+    document.querySelector(".navbar").classList.toggle("active")
+})
+let btnnav2 = document.getElementById("btn-nav2").addEventListener("click",() => {
+    document.querySelector(".navbar").classList.toggle("active")
+})
+
+
+
+
+
+
+
+
